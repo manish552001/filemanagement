2648582c2f7a468f59ec55bc08cdf00256c8b429
Manish Yadav <manishyadav552001@gmail.com>
2025-03-06 12:48:57 +0530

    Update UserFileRepository.java

--- src/main/java/com/unimate/filemanagement/repository/UserFileRepository.java
+++ src/main/java/com/unimate/filemanagement/repository/UserFileRepository.java
@@ -6,8 +6,9 @@ import org.springframework.stereotype.Repository;
 import com.unimate.filemanagement.model.UserFile;
 
 import java.util.List;
+import java.util.UUID;
 
 @Repository
-public interface UserFileRepository extends JpaRepository<UserFile, Long> {
+public interface UserFileRepository extends JpaRepository<UserFile, UUID> {
     List<UserFile> findByUserIdOrderByUploadedAtDesc(String userId);
 }
